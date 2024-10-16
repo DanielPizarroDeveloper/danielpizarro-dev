f4f7554d428b608506c5febbe1a397380b8b6a12
Daniel Pizarro Saavedra <Saavedra.Alejandro@outlook.com>
2024-10-16 20:08:16 -0300

    Actualización About Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
       <NavPage />
       <HeaderPage />
       <Experiencia />
-      <Skills />
+      {/* <Skills /> */}
       <AboutMe />
     </>
   )
--- src/Components/aboutMe.jsx
+++ src/Components/aboutMe.jsx
@@ -12,10 +12,10 @@ export function AboutMe() {
                     <img className='main-section-aboutMe-container__article__img' src="img/Avatar.jpg" alt="imagen de perfil del dueño del porfolio" />
                 </article>
 
-                <article>
+                <article style={{width: 'auto'}}>
                     <div className='main-section-aboutMe-container__article__container_presentacion'>
                         <div className='main-section-aboutMe-container__article__container__presentacion_h4'>
-                                <h4>Me presento, me llamo Daniel Pizarro Saavedra</h4>
+                            <h4>Me presento, me llamo Daniel Pizarro Saavedra</h4>
                         </div>
                     
                         <p className='main-section-aboutMe-container__article__container__presentacion__parrafo'>
